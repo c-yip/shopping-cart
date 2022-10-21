ad5f773c6dbf2b82021970020834998a7b935698
Christian <christianyip92@gmail.com>
2022-10-21 08:42:36 -0700

    Add links to store, wishlist, and cart pages.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
 import heart from "../images/heart.png";
 import shoppingCart from "../images/shopping-cart.png";
 
 export default function Header() {
   return (
     <header>
-      <h1 className="logo">Fake Store</h1>
+      <Link to="/">
+        <h1 className="logo">Fake Store</h1>
+      </Link>
 
+      {/* these will all be links to categories */}
       <nav className="nav-left">
         <h3>Shop</h3>
         <h3>Category 1</h3>
@@ -14,15 +18,19 @@ export default function Header() {
       </nav>
 
       <nav className="nav-right">
-        <div className="nav-icon-container">
-          <img src={heart} alt="Heart icon" />
-          <p>Wishlist</p>
-        </div>
+        <Link to="/wishlist">
+          <div className="nav-icon-container">
+            <img src={heart} alt="Heart icon" />
+            <p>Wishlist</p>
+          </div>
+        </Link>
 
-        <div className="nav-icon-container">
-          <img src={shoppingCart} alt="Shopping cart icon" />
-          <p>Cart</p>
-        </div>
+        <Link to="/cart">
+          <div className="nav-icon-container">
+            <img src={shoppingCart} alt="Shopping cart icon" />
+            <p>Cart</p>
+          </div>
+        </Link>
       </nav>
     </header>
   );
