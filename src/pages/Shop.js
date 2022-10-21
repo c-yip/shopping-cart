c57a48e65413153331c01a64d738b531b5f2fb38
Christian <christianyip92@gmail.com>
2022-10-21 16:30:24 -0700

    Edit product card content and styling.

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,21 +1,20 @@
 import useFetchData from "../hooks/useFetchData";
-
+import heart from "../images/heart.png";
 export default function Shop() {
   const { storeData } = useFetchData();
 
   const allProducts = storeData.map((item) => (
     <div className="product-card" key={item.id}>
-      <h3 className="product-title">{item.title}</h3>
-      <p className="product-description">{item.description}</p>
       <img className="product-image" src={item.image} alt={item.title} />
+      <p className="product-title">{item.title}</p>
       <p className="product-price">${item.price}</p>
       <div className="product-ratings">
-        <p className="product-rating">Rating: {item.rating.rate}</p>
-        <p className="product-rating-count">{item.rating.count}</p>
+        <p className="product-rating">Rating: {item.rating.rate}/5</p>
+        <p className="product-rating-count">({item.rating.count})</p>
       </div>
       <div className="product-buttons">
         <button className="product-button">Add to cart</button>
-        <button className="favorite-button">Add to favorites</button>
+        <img className="heart-icon" src={heart} alt="Favorite icon" />
       </div>
     </div>
   ));
