49bc0f8100aba5af622847b61ef672c9211649c7
Christian <christianyip92@gmail.com>
2022-10-21 08:33:04 -0700

    Install react-dom-router.
    Set up index and App for routes.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import "./App.css";
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
     <div className="App">
       <Header />
 
-      <main>
-        <h2>Products</h2>
-        <h2>Products</h2>
-        <h2>Products</h2>
-        <h2>Products</h2>
-        <h2>Products</h2>
-      </main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
 
       <Footer />
     </div>
