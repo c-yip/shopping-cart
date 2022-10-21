1ca92d53223acc6b90b5300d424a7e1963594ccf
Christian <christianyip92@gmail.com>
2022-10-21 12:20:40 -0700

    Add first section of Home page.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,25 @@
+import { Link } from "react-router-dom";
+import heroImage from "../images/woman-shopping-for-clothes.jpg";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="page home">
+      <div className="section1">
+        <img
+          src={heroImage}
+          alt="Woman shopping"
+          className="background-image"
+        />
+        <div className="hero overlay">
+          <h1>Fake items straight to your doorstep</h1>
+          <h3>
+            Take care of your fake items needs from the comfort of your home
+          </h3>
+          <Link to="/shop">
+            <div className="shop-button">Shop Now</div>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
