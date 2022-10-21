59457254967b8a37036d0f38236c2d0bf6f9c653
Christian <christianyip92@gmail.com>
2022-10-21 08:38:38 -0700

    Add wishlist page and route to wishlist.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
+import Wishlist from "./pages/Wishlist";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/wishlist" element={<Wishlist />} />
       </Routes>
 
       <Footer />
